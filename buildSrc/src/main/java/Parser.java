7ed534a3ccd8c185691479d0ea151b33d46b75c6
marcelo meneses <menesesmarcelo1@gmail.com>
2022-05-12 22:48:58 -0300

    Ajuste no Parser

--- buildSrc/src/main/java/Parser.java
+++ buildSrc/src/main/java/Parser.java
@@ -166,10 +166,10 @@ class Parser {
 
     private char fixup(int intChar) {
         switch (intChar) {
-            case '’':
+            case '\u2018':
                 return '\'';
-            case '”':
-            case '“':
+            case '\u201D':
+            case '\u201C':
                 return '\"';
             default:
                 return (char) intChar;
