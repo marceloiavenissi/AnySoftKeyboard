ca76e0485134a7586ace55c622a9d0ebe60f0b30
Marcelo Meneses <menesesmarcelo1@gmail.com>
2022-07-27 12:39:42 -0300

    test working

--- test_web_service/test_web_service.py
+++ test_web_service/test_web_service.py
@@ -1,12 +1,17 @@
-from flask import request
+from flask import Flask, request
+import sys
 
+app = Flask(__name__)
+
+# http POST http://127.0.0.1:5000/classifica text="Eu estou feliz" identificador="xxxx" datetime="0"
 @app.route('/classifica', methods=['POST'])
 def classifica():
     if request.method == 'POST':
-        text = request.form['text']
-        identificador = request.form['identificador']
-        datetime = request.form['datetime']
+        text = request.args['text']
+        identificador = request.args['identificador']
+        datetime = request.args['datetime']
 
-        print(identificador)
-        print(text)
-        print(datetime)
+        msg = f'{datetime} {identificador} --> {text}'
+
+        print(msg, file=sys.stderr)
+    return ''
